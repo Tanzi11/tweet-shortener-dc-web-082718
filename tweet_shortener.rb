0ff52d12f909dde77566c97b6c41d735893a073b
Tanzi11 <tanzima.c@fellows.thedoschool.org>
2018-08-07 23:06:52 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -33,5 +33,9 @@ def shortened_tweet_truncator(tweet)
   if word_substituter(tweet).length > 140
     word_substituter(tweet)[0..136] + ("...")
   else
+    word_substituter(tweet)
+  end
+end
+
       
   
